b09cbabc0d31cab4d1b48f812d5111e952b7b94b
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-10-12 12:10:17 +0600

    add to toast

---
+++ src/components/ui/Toast.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+interface ToastProps {
+  open: boolean;
+  handleClose: () => void;
+  message: string;
+}
+
+export default function Toast({ open, handleClose, message }: ToastProps) {
+  return (
+    <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
