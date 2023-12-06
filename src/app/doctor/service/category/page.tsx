3f3c0a0891b1616926568f8620c3ca65fb1ad254
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-06 23:41:30 +0600

    bug fixed  category service page

--- src/app/doctor/service/category/page.tsx
+++ src/app/doctor/service/category/page.tsx
@@ -2,6 +2,7 @@
 import DoctorService from "@/components/doctorService/DoctorService";
 import NoData from "@/components/ui/NoData";
 import { useAllServiceQuery } from "@/redux/api/doctorServiceApi";
+import LoadingSpinner from "@/utils/Loading";
 import { Pagination } from "@mui/material";
 import React, { useState } from "react";
 
@@ -22,16 +23,20 @@ const ServiceCategoryPage = ({
   const handlePageChange = (event: any, page: any) => {
     setCurrentPage(page);
   };
-  const { data } = useAllServiceQuery({ ...query });
+  const { data, isLoading } = useAllServiceQuery({ ...query });
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
   return (
-    <div className="max-w-7xl mx-auto lg:px-4 mt-28 ">
+    <div className="max-w-7xl mx-auto px-4 lg:px-0 mt-28 ">
       {data?.data?.length === 0 ? (
         <NoData />
       ) : (
-        <div className="borde   rounded p-5  relative shdow  h-[540px] mt-5">
-          <h3 className=" text-3xl font-bold">Doctor Service</h3>
+        <div className="   rounded p-5 lg:p-0  relative  h-[540px] mt-5">
+          <h3 className=" text-3xl font-bold">Doctor Services</h3>
           <div className=" flex gap-5">
-            <div className="grid lg:grid-cols-2  grid-cols-1 gap-5 mt   ">
+            <div className="grid lg:grid-cols-3  grid-cols-1 gap-10 mt   ">
               {data?.data?.map((service: any) => (
                 <DoctorService key={service?.id} service={service} />
               ))}
