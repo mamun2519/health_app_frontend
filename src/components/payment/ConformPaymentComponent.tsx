341f1743fed85bf79d506c82abb5f8e6d6844254
Juboraj Islam Mamun <juborajvai22@gmail.com>
2024-01-25 12:32:24 +0600

    add to metadata in payment page

---
+++ src/components/payment/ConformPaymentComponent.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "@/components/Form/CheckoutForm";
+import MyStepper from "@/components/ui/MyStepper";
+import { usePaymentStripeQuery } from "@/redux/api/paymentApi";
+import LoadingSpinner from "@/utils/Loading";
+import PaymentPic from "@/assets/paymentCard.png";
+import Image from "next/image";
+import Link from "next/link";
+
+// const stripePromise = loadStripe(
+//   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+// );
+const stripePromise = loadStripe(
+  "pk_test_51L1nmNCGpaTt0RU8npNSNITrjLTAUDjwjX275RD6RDk5SGoYi1H1zLKxAis8OFp4C0PxQBT2L5c0L0VsTI9ewqGl00dT2UHEXy"
+);
+const ConformPageComponent = () => {
+  const [price, setPrice] = useState<number>(0);
+  useEffect(() => {
+    setPrice(JSON.parse(localStorage.getItem("BookingInfo") as string).price);
+  }, []);
+
+  const { data, isLoading } = usePaymentStripeQuery(price);
+  console.log(price);
+  const appearance = {
+    theme: "stripe",
+  };
+  const options: {
+    clientSecret: string;
+    appearance: any;
+  } = {
+    clientSecret: data?.clientSecret,
+    appearance,
+  };
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 lg:px-0 mt-28 pb-40">
+      <MyStepper stepper={2} />
+      <div className="  lg:flex gap-5">
+        <div className="border h-[550px] lg:w-1/2   w-full rounded-2xl p-7 mt-10">
+          {data?.clientSecret && (
+            <Elements options={options} stripe={stripePromise}>
+              <div className="mt-10">
+                <CheckoutForm />
+              </div>
+            </Elements>
+          )}
+        </div>
+        <div className=" lg:w-1/2  w-full border  p-7 rounded-3xl shadow-sm  h-[550px]  mt-10">
+          <Image
+            src={PaymentPic}
+            width={500}
+            height={520}
+            className="w-full h-96"
+            alt="Pic"
+          />
+          <div className=" flex justify-end">
+            <div className="mt-20 w-48 h-10 rounded bg-[#d1001c] text-white  font-medium  flex justify-center items-center">
+              <Link href="/payment/preview">Back</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConformPageComponent;
---
+++ src/components/payment/SuccessPageComponent.tsx
@@ -0,0 +1,99 @@
+"use client";
+import MyStepper from "@/components/ui/MyStepper";
+import React, { useEffect, useState } from "react";
+import SuccessPayment from "@/assets/paymentDone.png";
+import Image from "next/image";
+
+import { useRouter } from "next/navigation";
+import { useCreatePaymentMutation } from "@/redux/api/paymentApi";
+const SuccessPageComponent = () => {
+  const [counter, setCounter] = useState(4);
+
+  const [createPayment] = useCreatePaymentMutation();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const bookingInfo = localStorage.getItem("BookingInfo");
+      const patientInfo = localStorage.getItem("PatientInfo");
+
+      if (bookingInfo && patientInfo) {
+        const price = JSON.parse(bookingInfo);
+        const patient = JSON.parse(patientInfo);
+
+        const appointment = {
+          bookingDate: price?.bookingDate,
+          doctorId: price?.doctorId,
+          gender: patient?.gender,
+          age: Number(patient?.age),
+          weight: Number(patient?.weight),
+          bloodGroup: patient?.bloodGroup,
+          slatTime: price?.slatTime,
+          patientProblem: patient?.patientProblem,
+          report: "no",
+          address: patient?.address,
+          serviceId: price?.serviceId,
+          price: String(price.price),
+        };
+        console.log(appointment);
+
+        const payment = {
+          serviceId: price.serviceId,
+          price: Number(price.price),
+          doctorId: price.doctorId,
+          transactionId: "No",
+          discountedPrice: Number(price.discount),
+          paymentType: "Stripe",
+        };
+
+        const paymentSuccess = async () => {
+          const res = await createPayment({ appointment, payment }).unwrap();
+
+          if (res) {
+            localStorage.removeItem("BookingInfo");
+            localStorage.removeItem("PatientInfo");
+          }
+        };
+
+        paymentSuccess();
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (counter > 0) {
+        setCounter(counter - 1);
+      } else {
+        clearInterval(interval);
+        router.push("/dashboard");
+      }
+    }, 1000);
+  }, [counter, router]);
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 lg:px-0 mt-28 pb-40">
+      <MyStepper stepper={3} />
+      <div className="border h-[550px] lg:w-1/2   w-full rounded-2xl p-7 mt-10 mx-auto">
+        <Image
+          src={SuccessPayment}
+          width={500}
+          height={520}
+          className="w-full h-96"
+          alt="Pic"
+        />
+
+        <div className="flex justify-center">
+          <div className="text-center">
+            <h3>Payment Complete Successfully</h3>
+            <p>Wait {counter}</p>
+            <p>Redirect To Dashboard</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessPageComponent;
